bac84d2129bed0d23ba16c11d83733539579afdf
shubhalaxmipatil <82553864+shubhalaxmipatil@users.noreply.github.com>
2023-01-12 14:23:26 +0530

    ecommerse started

---
+++ JS/JavaScriptssss.js
@@ -0,0 +1,12 @@
+function fun1(){
+    alert("I am not ready");
+}
+function fun2(){
+    alert("Prearing.............");
+}
+function fun3(){
+    alert("Prearing.............");
+}
+function fun4(){
+    alert("Prearing.............");
+}
